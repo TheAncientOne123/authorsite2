d61efa4c964f07a7159b724dc16ab990f8d49ed5
TheAncientOne123 <calkestisxdq@gmail.com>
2025-08-18 22:13:49 -0600

    config modified

--- hectorsanchez-site/docusaurus.config.js
+++ hectorsanchez-site/docusaurus.config.js
@@ -38,21 +38,22 @@ const config = {
   title: 'El Códice',
   favicon: 'img/favicon.ico',
 
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://TheAncientOne123.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TheAncientOne123', // Usually your GitHub org/user name.
+  projectName: 'authorsite2', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
